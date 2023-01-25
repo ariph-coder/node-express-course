06982a83abfd2f28f77f126ed0d66a41cfbdfd4a
Arifudin <ariph1994@gmail.com>
2023-01-25 13:08:19 +0700

    mengerjakan task manager

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,31 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/task')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+
+//midleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+
+//routess
+app.get('/hello', (req,res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks',tasks)
+app.use(notFound)
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
